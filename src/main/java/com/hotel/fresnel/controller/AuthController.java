2ef1278067a85850074196482a271c4275e1523a
billfres <ngomafresnel@gmail.com>
2024-02-27 21:58:29 +0100

    test message

--- src/main/java/com/hotel/fresnel/controller/AuthController.java
+++ src/main/java/com/hotel/fresnel/controller/AuthController.java
@@ -17,10 +17,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -61,5 +58,10 @@ public class AuthController {
                 userDetails.getEmail(),
                 jwt,
                 roles));
+    }
+
+    @GetMapping("/")
+    public String test(){
+        return "c'est un test";
     }
 }
