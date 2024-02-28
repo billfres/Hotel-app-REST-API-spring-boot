acd53c1c0679c0e450a6a9b903d0e20dc1cf27f0
billfres <ngomafresnel@gmail.com>
2024-02-28 03:16:01 +0100

    new origin 2

--- src/main/java/com/hotel/fresnel/security/WebSecurityConfig.java
+++ src/main/java/com/hotel/fresnel/security/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig {
                         exception -> exception.authenticationEntryPoint(jwtAuthEntryPoint))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("alleged-money-production.up.railway.app/auth/**", "/rooms/**","/bookings/**")
+                        .requestMatchers("alleged-money-production.up.railway.app/auth/**", "/auth/**", "/rooms/**","/bookings/**")
                         .permitAll().requestMatchers("/roles/**").hasRole("ADMIN")
                         .anyRequest().authenticated());
         http.authenticationProvider(authenticationProvider());
